0f9db9f758d1b13cb7d8fc2fe6364e4512c9615f
Krzysztof9911 <k.wiesniakowski@student.utwente.nl>
2021-06-02 20:23:58 +0200

    oh my god, I think I managed to parse the file from the user, fingers crossd for that pls!!!!!!!!!

--- src/main/java/UploadServlet.java
+++ src/main/java/UploadServlet.java
@@ -15,6 +15,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Paths;
+import java.sql.Connection;
 import java.sql.SQLException;
 
 
@@ -25,22 +26,19 @@ public class UploadServlet extends HttpServlet {
     MT940 mt940;
     ConnectionHandler connectionHandler;
     public UploadServlet() throws IOException, SQLException {
+
     }
 
 
     // ...
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//     /   String description = request.getParameter("description"); // Retrieves <input type="text" name="description">
         Part filePart =  request.getPart("file"); // Retrieves <input type="file" name="file">
-//        System.out.println("this is file part:" + filePart.getSubmittedFileName());
-
-//        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
         InputStream inputStream = null;
         if (filePart != null) {
-//            long fileSize = filePart.getSize();
-//            String fileContent = filePart.getContentType();
-//            inputStream = filePart.getInputStream();
-            mt940 = new MT940(new File("/upload"));
+            long fileSize = filePart.getSize();
+            String fileContent = filePart.getContentType();
+            mt940 = new MT940(filePart.getInputStream());
             if (mt940 != null) {
                 String text = "start";
                 if (mt940.getField20().getValue() != null) {
@@ -48,7 +46,7 @@ public class UploadServlet extends HttpServlet {
                 }
                 try {
                     connectionHandler = new ConnectionHandler();
-                    connectionHandler.insertMethod(2, text, 10, 23, "ddd", "dddd", "ssddd", 'D');
+                    connectionHandler.insertMethod(2, text, 10, 23, "2019-11-02", "2019-11-04", "ssddd", 'D');
                 } catch (SQLException throwables) {
                     throwables.printStackTrace();
                 }
